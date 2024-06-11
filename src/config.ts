654c9274cbc34bfc81af19a627cf9608fce19972
sang <sangpro123451@gmail.com>
2024-06-12 00:03:58 +0700

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://127.0.0.1:3001";
-// export const BASE_URL = "https://pbl7-backend.onrender.com";
+// export const BASE_URL = "http://127.0.0.1:3001";
+export const BASE_URL = "https://pbl7-backend.onrender.com";
