7e05f555e5234ba8af9ab103fcc9016b36a88114
sang <sangpro123451@gmail.com>
2024-06-15 09:07:10 +0700

    change url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://127.0.0.1:3001";
 // export const BASE_URL = "https://pbl7-backend.onrender.com";
-export const BASE_URL = "https://c0f5-117-2-255-206.ngrok-free.app";
+export const BASE_URL = "https://e1a5-117-2-255-206.ngrok-free.app/";
